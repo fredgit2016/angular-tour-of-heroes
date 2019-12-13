4c55ceab30f858a44f0bb71928a13319cbeb655a
Frederic LE GAC <flegac@sii.fr>
2019-12-13 10:10:35 +0100

    (feat): angular routing tutorial

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,9 +8,16 @@ import { MessageService } from '../message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageService: MessageService) { }
+  constructor(public messageService: MessageService) {}
 
   ngOnInit() {
   }
 
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
